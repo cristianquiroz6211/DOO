375e9e79abff7c3cc9ed2bca05ebeb2a0193ce3d
cristianquiroz6211 <Cristian.quiroz6211@uco.net.co>
2024-05-08 17:28:43 -0500

    mensajes
    Se agrega catalogo

--- TiendaChepito/src/main/java/co/edu/uco/tiendachepito/crosscutting/helpers/ObjectHelper.java
+++ TiendaChepito/src/main/java/co/edu/uco/tiendachepito/crosscutting/helpers/ObjectHelper.java
@@ -1,22 +1,22 @@
 package co.edu.uco.tiendachepito.crosscutting.helpers;
-public class ObjectHelper {
-    private static final ObjectHelper instance = new ObjectHelper();
 
-    private ObjectHelper() {
-        super();
-    }
+public final class ObjectHelper {
+	private static final ObjectHelper instance = new ObjectHelper();
 
-    public static final ObjectHelper getObjectHelper() {
-        return instance;
-    }
+	private ObjectHelper() {
+		super();
+	}
 
-    public final <T> boolean isNull( final T object) {
-        return object == null;
-    }
+	public static final ObjectHelper getObjectHelper() {
+		return instance;
+	}
 
-    public final <T> T getDefault(final T object, final T defaultObject) {
-        return isNull(object) ? defaultObject : object;
-    }
+	public final <T> boolean isNull(final T object) {
+		return object == null;
+	}
 
+	public final <T> T getDefault(final T object, final T defaultObject) {
+		return isNull(object) ? defaultObject : object;
+	}
 
 }
